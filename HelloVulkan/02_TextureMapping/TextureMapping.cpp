4451dbaf1e666e6ca010d0aa5f5aa7434a530846
Kirkice <kirkhan1@outlook.com>
2022-11-14 21:47:08 +0800

    .

--- HelloVulkan/02_TextureMapping/TextureMapping.cpp
+++ HelloVulkan/02_TextureMapping/TextureMapping.cpp
@@ -742,7 +742,7 @@ private:
 		createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
 		void* data;
-		vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+		vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);  
 		memcpy(data, pixels, static_cast<size_t>(imageSize));
 		vkUnmapMemory(device, stagingBufferMemory);
 
