19e4455955c67e4094f68b8d46592debc2f892cf
Kirkice <kirkhan1@outlook.com>
2022-11-17 22:16:14 +0800

    .

--- HelloVulkan/02_TextureMapping/TextureMapping.cpp
+++ HelloVulkan/02_TextureMapping/TextureMapping.cpp
@@ -623,7 +623,7 @@ private:
 		VkPipelineRasterizationStateCreateInfo rasterizer{};
 		rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
 		rasterizer.depthClampEnable = VK_FALSE;
-		rasterizer.rasterizerDiscardEnable = VK_FALSE;
+		rasterizer.rasterizerDiscardEnable = VK_FALSE; 
 		rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
 		rasterizer.lineWidth = 1.0f; 
 		rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
