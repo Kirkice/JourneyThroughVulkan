32d9c79bd9e94072a43bac42f5446a12b6dbce7a
Kirkice <kirkhan1@outlook.com>
2022-11-20 17:00:09 +0800

    TextureMapping

--- HelloVulkan/02_TextureMapping/TextureMapping.cpp
+++ HelloVulkan/02_TextureMapping/TextureMapping.cpp
@@ -610,7 +610,7 @@ private:
 		vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
 		vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 
-		VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
+		VkPipelineInputAssemblyStateCreateInfo inputAssembly{}; 
 		inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
 		inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
 		inputAssembly.primitiveRestartEnable = VK_FALSE;
